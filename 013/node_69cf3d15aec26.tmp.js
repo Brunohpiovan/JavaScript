2bda0eccd38c3ce1d564e3ceb91a556ac13f7ac4
Bruno Piovan <bpiovan90@gmail.com>
2024-09-06 10:17:52 -0300

    repeticoes

---
+++ 013/node_69cf3d15aec26.tmp.js
@@ -0,0 +1,20 @@
+
+var c=0
+while(c<5){
+    console.log('oi')
+    c++
+}
+
+do{
+    var c = console.log('voce deseja continuar o programa?(0 - sair)(1-continuar)')
+}while(c != 0)
+
+
+
+/*
+console.log('oi')
+console.log('oi')
+console.log('oi')
+console.log('oi')
+console.log('oi')
+*/
